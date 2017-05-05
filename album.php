26589e5087eddf8952711e85d1af115cf257f819
Greig Christie <greig.christie@ed.ac.uk>
2017-05-05 09:58:19 +0100

    The new everything artist and album pages

---
+++ album.php
@@ -0,0 +1,84 @@
+<?php
+include("functions.php");
+include("connected.php");
+header('Content-Type:text/html; charset=UTF-8');
+		$sqlc = "select count(id) from albums where album_owned = 1";
+		//echo $sqlc;
+		$resultc = mysqli_query($con,$sqlc);
+		while ($row = mysqli_fetch_row($resultc))
+		{
+		$noalbums = $row[0];
+		}
+		$offset = 50;
+		
+		include("header.php");
+?>
+
+<h2>All <?php echo $noalbums ?> albums <small>(<?php echo $offset; ?> per page)</small></h2>
+
+<?php
+$nopages = round($noalbums / $offset);
+
+//echo "Number of pages should be : ". $nopages;
+if (isset($_GET['page'])){
+$page = $_GET['page'];
+} else {
+$page = 1;
+}
+if ($page > $nopages) {
+echo "whoa nelly, no such page!";
+} else {
+$prevpage = $page - 1;
+
+
+$top = $page * 100;
+$bottom = $prevpage * $offset;
+echo pubpag($page, $noalbums, $heres, $offset);
+?>
+
+<p class="lead">Album</p>
+<?php
+//Just to see what comments look like!
+		$sql = "select a.artist_name, b.id, b.album_title, b.album_collection";
+		$sql = $sql . " from artists a, albums b";
+		$sql = $sql . " where a.id = b.artist_id";
+		$sql = $sql . " and b.album_owned = 1";
+		$sql = $sql . " order by b.album_title";
+		$sql = $sql . " LIMIT $offset offset $bottom";
+//		echo $sql;
+		$result = mysqli_query($con,$sql);
+		$row_cnt = mysqli_num_rows($result);
+		while ($row = mysqli_fetch_array($result))
+		{
+//		print_r($row);
+		//$trackartist = $row['trackartist'];
+		//$trackname = $row['trackname'];
+		$trackalbum = $row['album_title'];
+		$albumid = $row['id'];
+		$albumartist = $row['artist_name'];
+		$albumcollection = $row['album_collection'];
+		//$utrackartist = urlencode($trackartist);
+		//$utrackname = urlencode($trackname);
+		$utrackalbum = urlencode($trackalbum);
+			echo "<div id ='row'>";
+			echo "<div id='col-xs-8'>";
+			echo "<p><a href='albumview.php?req=albumid&query=$albumid'>$trackalbum</a> - $albumartist ($albumcollection) <i class=\"fa fa-plus-square\" style='font-size: 12pt;' onclick='showUser($albumid)' id='openList$albumid'></i><i class=\"fa fa-minus-square\" style='font-size: 12pt; display: none;' onclick='hideDiv($albumid)' id='closeList$albumid'></i></p>";
+			echo "<div class='row'><div class='col-xs-12' id='trackCon$albumid'></div></div>\n";
+			//echo "<td>" . $trackname . "</td> ";
+			//echo "<td>" . $trackalbum . "</td> ";
+			echo "</div>\n";
+			echo "</div>\n";
+		}
+	}//nelly	
+?>
+
+
+<?php
+echo totop($row_cnt);
+echo pubpag($page, $noalbums, $heres, $offset);
+?>
+
+<?php
+include("footer.php");
+?>
+
---
+++ artist.php
@@ -0,0 +1,85 @@
+<?php
+include("functions.php");
+include("connected.php");
+		$sqlc = "select count(distinct(a.id))";
+		$sqlc = $sqlc . " from artists a, albums b, tracks c";
+		$sqlc = $sqlc . " where a.id = c.track_artist_id";
+		$sqlc = $sqlc . " and b.id = c.album_id";
+		$sqlc = $sqlc . " and b.album_owned = 1";
+		//echo $sqlc;
+		$resultc = mysqli_query($con,$sqlc);
+		while ($row = mysqli_fetch_row($resultc))
+		{
+		$noalbums = $row[0];
+		}
+	include("header.php");
+	$offset = 35;
+?>
+
+<h2>All <?php echo $noalbums ?> artists <small>(<?php echo $offset; ?> per page)</small></h2>
+
+<?php
+
+
+$nopages = ceil($noalbums / $offset);
+
+//echo "Number of pages should be : ". $nopages;
+if (isset($_GET['page'])){
+$page = $_GET['page'];
+} else {
+$page = 1;
+}
+if ($page > $nopages) {
+echo "whoa nelly, no such page!";
+} else {
+$prevpage = $page - 1;
+
+
+$top = $page * 100;
+$bottom = $prevpage * $offset;
+echo pubpag($page, $noalbums, $heres, $offset);
+?>
+
+<p class="lead">Artist</p>
+<?php
+
+//Just to see what comments look like!
+		$sql = "select a.id, a.artist_name";
+		$sql = $sql . " from artists a, albums b, tracks c";
+		$sql = $sql . " where a.id = c.track_artist_id";
+		$sql = $sql . " and b.id = c.album_id";
+		$sql = $sql . " and b.album_owned = 1";
+		$sql = $sql . " group by a.id";
+		$sql = $sql . " order by artist_sort_name";
+		$sql = $sql . " LIMIT $offset offset $bottom";
+		//$sql = $sql . " group by trackartist";
+		$result = mysqli_query($con,$sql);
+		$row_cnt = mysqli_num_rows($result);
+		while ($row = mysqli_fetch_array($result))
+		{
+		$artistid = $row['id'];
+		$trackartist = $row['artist_name'];
+		//$trackname = $row['trackname'];
+		//$trackalbum = $row['trackalbum'];
+		$utrackartist = urlencode($trackartist);
+		//$utrackname = urlencode($trackname);
+		//$utrackalbum = urlencode($trackalbum);
+			echo "<div id ='row'>";
+			echo "<div id='col-xs-8'><p><a href='refined.php?req=trackartist&query=$artistid&sort=albumartist'>$trackartist</a> - <a href='artistview.php?artist=$artistid'>View Albums</a></p></div>";
+			//echo "<td><a href='refined.php?req=trackname&query=$utrackname'>$trackname</a></td>";
+			//echo "<td><a href='refined.php?req=trackalbum&query=$utrackalbum'>$trackalbum</a></td>";
+			//echo "<td>" . $trackname . "</td> ";
+			//echo "<td>" . $trackalbum . "</td> ";
+			echo "</div>\n";
+		}
+		} // nelly?
+?>
+</tbody>
+</table>
+
+<?php
+echo totop($row_cnt);
+echo pubpag($page, $noalbums, $heres, $offset);
+
+include("footer.php");
+?>
